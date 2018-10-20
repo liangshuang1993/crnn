37004645823b7c9f263cab29aa2123a68b1ddfe7
liangshuang4 <liangshuang4@lenovo.com>
2018-10-20 18:50:30 +0800

    this version is a clean version

---
+++ densent_ocr/convert_label.py
@@ -0,0 +1,29 @@
+from io import open
+dicts = ''
+with open('/datasets/text_renderer/data/chars/med.txt', encoding='utf-8') as f:
+    for ch in f.readlines():
+        ch = ch.strip()
+        dicts=dicts+ch
+
+char_to_id = {j: i for i,j in enumerate(dicts)}
+#print char_to_id
+
+DATASETS = '/datasets/text_renderer/val1'
+
+with open(DATASETS + '/default/tmp_labels.txt', encoding='utf-8') as f:
+#with open('/datasets/TextRecognitionDataGenerator/TextRecognitionDataGenerator/train2/labels.txt', encoding='utf-8') as f:
+    lines = f.readlines()
+
+#with open('/datasets/TextRecognitionDataGenerator/TextRecognitionDataGenerator/train2.txt', 'wb') as f:
+with open(DATASETS + '/default/tmp_labels_id.txt', 'wb') as f:
+    for line in lines:
+        image, label = line.strip().split(' ')
+        f.write(image + '.jpg')
+        for char in label:
+            try:
+                f.write(' ' + str(char_to_id[char] + 1))
+            except Exception as e:
+                print char
+        f.write('\n')
+
+1
